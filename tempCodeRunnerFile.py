328976ed17deace7915fe234c674cc4893eac780
rimmykrigupta <rimmygupta241@gmail.com>
2024-09-26 16:47:48 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
-            print(f"Google Speech Recognition error; {e}")
